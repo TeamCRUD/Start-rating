4567dc2847094d2823f4923a0328375e2682dba1
Jair Pérez <jair.perez@outlook.com>
2017-04-29 13:21:27 -0500

    Init First commit

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+     port: process.env.PORT || '3000',
+     db: process.env.MONGODB || 'mongodb://teamcrud:D1e560*9c@ds121171.mlab.com:21171/creators-test',
+     SECRET_TOKEN: 'miclavedetokens'
+}
---
+++ routes/api.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const express = require('express')
+const producCtrl = require('../controllers/product')
+const userCtrl = require('../controllers/user')
+const auth = require('../middlewares/auth')
+const api = express.Router()
+
+api.get('/product', producCtrl.getProducts)
+api.get('/product/:productId', producCtrl.getProduct)
+api.post('/product', auth, producCtrl.saveProduct)
+api.put('/product', auth, producCtrl.updateProduct)
+api.delete('/product', auth, producCtrl.deleteProduct)
+api.post('/signup', userCtrl.signUp)
+api.post('/signin', userCtrl.signIn)
+api.get('/private', auth, (req, res) => {
+  res.status(200).send({ message: 'Tienes acceso'})
+})
+
+module.exports = api
